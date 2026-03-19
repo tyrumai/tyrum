38ba60e8ced120002c9e63447aa57741c9fedee7
Ron Hernaus <ron@virtunet.nl>
2026-03-19 14:51:48 +0100

    fix: narrow operator core node exports

--- packages/operator-core/src/node.ts
+++ packages/operator-core/src/node.ts
@@ -1,3 +1,27 @@
 export * from "./index.js";
-export * from "@tyrum/node-sdk/node";
-export type * from "@tyrum/node-sdk/node";
+export { autoExecute, createManagedNodeClientLifecycle } from "@tyrum/node-sdk/node";
+export {
+  TyrumClient,
+  normalizeFingerprint256,
+  createDeviceIdentity,
+  createNodeFileDeviceIdentityStorage,
+  createPinnedNodeTransportState,
+  createPinnedNodeWebSocket,
+  destroyPinnedNodeDispatcher,
+  createTyrumHttpClient,
+  loadOrCreateDeviceIdentity,
+} from "@tyrum/transport-sdk/node";
+export type {
+  CapabilityProvider,
+  ManagedNodeClientLifecycle,
+  TaskResult,
+} from "@tyrum/node-sdk/node";
+export type { ExecutionAttempt, ExecutionRun, ExecutionStep, MemoryItem } from "@tyrum/client/node";
+export type {
+  DeviceIdentity,
+  NodePinnedTlsOptions,
+  NodePinnedTransportState,
+  NodePinnedWebSocketOptions,
+  NodeTyrumClientOptions,
+  NodeTyrumHttpClientOptions,
+} from "@tyrum/transport-sdk/node";
